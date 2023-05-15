cc8b1437fc340948b0c2c4786dc59a1ba10c1679
wuliao97 <117802193+wuliao97@users.noreply.github.com>
2023-05-15 16:30:08 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var left = Convert.ToInt32(textBox1.Text);
+            var right = Convert.ToInt32(textBox2.Text);
+
+            textBox3.Text = Convert.ToString((left + right));
+            textBox4.Text = Convert.ToString(left - right);
+            textBox5.Text = Convert.ToString(left * right);
+            textBox6.Text = Convert.ToString(left / right);
+
+        }
+
+
+    }
+}
